9e24bb6e2d93a7b14b254da12e7df855326ed4ef
osobambam <dejioso20@gmail.com>
2024-08-14 10:47:34 +0100

    Changed the buttons to show when edit button is changed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -89,9 +89,14 @@ export default function TodoList(props){
                     />    
                     )
                 }
-                <button className="button-complete task-button" onClick={()=>handleComplete(storeItem.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>                
-                </button>
+                {editId === storeItem.id ?
+                    ("") :
+                    (
+                        <button className="button-complete task-button" onClick={()=>handleComplete(storeItem.id)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>                
+                        </button>
+                    )
+                }
                 {editId === storeItem.id ?
                     (
                         <button className="button-edit task-button" onClick={()=>saveEdit(storeItem.id)}>
